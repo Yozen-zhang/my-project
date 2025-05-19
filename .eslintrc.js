2042dc7a979605fc88abfe1b9841d43b1eabf4db
Yozen-zhang <mkyozen918043@gail.com>
2025-05-19 16:00:58 +0800

    優化

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,24 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    browser: true,
+    es2021: true,
   },
   extends: [
     'plugin:vue/vue3-essential',
-    '@vue/standard',
-    'eslint:recommended', 
-    'prettier'
+    'eslint:recommended',
+    'plugin:prettier/recommended',  // 用 prettier 官方推薦整合方式
   ],
   parserOptions: {
-    parser: '@babel/eslint-parser'
+    parser: '@babel/eslint-parser',
+    ecmaVersion: 12,
+    sourceType: 'module',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/multi-word-component-names': 'off',  // 可依需求開啟/關閉
+  },
 }
+
--- src/methods/filters.js
+++ src/methods/filters.js
@@ -1,8 +1,9 @@
+// 千分號 
 export function currency(num) {
   const n = parseInt(num, 10);
-  return `${n.toFixed(0).replace(/./g, (c, i, a) => (i && c !== '.' && ((a.length - i) % 3 === 0) ? `, ${c}`.replace(/\s/g, '') : c))}`;
+  return n.toLocaleString();
 }
-
+// 日期轉化 
 export function date(time) {
   const localDate = new Date(time * 1000);
   return localDate.toLocaleDateString();
